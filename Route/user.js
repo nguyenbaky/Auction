aa0830117fddeeaaea4e60eb4a5f6be716f27e68
nguyenbaky <baky12lhp@gmail.com>
2019-12-07 21:02:31 +0700

    profile

--- Route/user.js
+++ Route/user.js
@@ -3,12 +3,56 @@ var router = express.Router()
 const Users = require("../models/User")
 const Cates = require("../models/cate")
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 //profile
 router.get("/profile/:userID",async function(req,res){
     var user,cates
     await Users.findOne({_id:res.locals.id}).then(u => user = u)
     await Cates.find({}).then(c => cates = c)
     res.render("home",{page:"profile",user,cates});
+})
+
+router.post("/profile/:userID",async function(req,res) {
+    var {username,ho_ten,email,dia_chi} = req.body
+    var id =  req.params.userID
+    await Users.findOne({_id:id},function (err,user) {
+        if(err) return res.send(err)
+        else {
+            Users.findOne({username},function (err,user) {
+                if(user) return res.send("Username đã tồn tại")
+                else {
+                    Users.findOne({email},function (err,u) {
+                        if(u) return res.send("Email đã tồn tại")
+                        else {
+                            Users.findOneAndUpdate({_id:id},
+                                {username,ho_ten,email,dia_chi},
+                                function(err,u){
+                                    if(err) return res.send(err)
+                                    else return res.send("Update thành công")
+                            })
+                        }
+                    })
+                }                
+            })
+        }        
+    })
+})
+
+router.put("change_password/:userID",async function(req,res){
+    var {oldpassword,password} = req.body
+    bcrypt.hash(oldpassword,saltRounds,function(err,hash) {
+        oldpassword = hash
+    })
+    bcrypt.hash(password,saltRounds,function(err,hash){
+        password = hash
+    })
+    var id = req.params.userID
+    await Users.findOneAndUpdate({_id:id,password:oldpassword},{password},function(err,user){
+        if(err) return res.send(err)
+        else return res.send("Đổi password thành công")
+    })
 })
 
 // /favorite/:user
---
+++ public/js/profile.js
@@ -0,0 +1,59 @@
+$( document ).ready(function() {    
+    $("#save").click(function(e){
+        var data = {}
+        data.username = $("#user_name").text()
+        data.ho_ten = $("#name").text()
+        data.email = $("#email").text()
+        data.dia_chi = $("#address").text()
+        if(data.username === "" || data.ho_ten === "" || data.email === "" || data.dia_chi === ""){
+            alert("Nhập đủ thông tin")
+            return
+        }  
+        var url = window.location.pathname.split("/")
+        id = url[2]     
+        $.ajax({
+            type: 'PUT',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            url: '/profile/'+id,						
+            success: function(data) {
+                alert(data);                       
+            }
+        });
+    })
+
+    $("#reset").click(function(e){
+        $("#user_name").val("")
+        $("#name").val("")
+        $("#email").val("")
+        $("#address").val("")
+    })
+
+    $("#change").clink(function(e){
+        var oldpassword = $("#oldpassword").text()
+        var newpassword = $("#newpassword").text()
+        var confirmnewpassword = $("#confirmnewpassword").text()
+        if(oldpassword === "" || newpassword === "" || confirmnewpassword === ""){
+            alert("Nhập đủ thông tin")
+            return 
+        }
+        if(newpassword !== confirmnewpassword){
+            alert("password not match")
+            return
+        }
+        var data = {}
+        data.password = newpassword
+        data.oldpassword = oldpassword
+        var url = window.location.pathname.split("/")
+        id = url[2] 
+        $.ajax({
+            type: 'PUT',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            url: '/change_password/'+id,						
+            success: function(data) {
+                alert(data);                       
+            }
+        });
+    })
+})
